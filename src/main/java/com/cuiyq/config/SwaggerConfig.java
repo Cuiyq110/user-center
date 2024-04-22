1527a49f502f411c29b603f69d326b2649486c87
cuiyq <cuiyq107@gmail.com>
2024-04-22 09:42:04 +0800

    add:整合 Swagger + Knife4j 接口文档

---
+++ src/main/java/com/cuiyq/config/SwaggerConfig.java
@@ -0,0 +1,52 @@
+package com.cuiyq.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * @version V1.0
+ * @Title:
+ * @Description:
+ * @Copyright 2024 Cuiyq
+ * @author: Cuiyq
+ * @date: 2024/4/20 15:24
+ */
+@Configuration
+@EnableSwagger2WebMvc
+@Profile({"dev", "test"})   //版本控制访问
+public class SwaggerConfig {
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                // 这里一定要标注你控制器的位置
+                .apis(RequestHandlerSelectors.basePackage("com.cuiyq.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * api 信息
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("伙伴匹配系统")
+                .description("伙伴匹配系统接口文档")
+                .termsOfServiceUrl("https://github.com/Cuiyq110")
+                .contact(new Contact("cuiyq","http://user-front.cuiyq.top/","cuiyq107@gmail.com"))
+                .version("1.0")
+                .build();
+    }
+}
